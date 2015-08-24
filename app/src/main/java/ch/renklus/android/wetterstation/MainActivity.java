5241f19f4c4197e9037316e68d4282d659357650
renklus <renklus.dev@outlook.com>
2015-08-25 00:25:35 +0200

    Initial commit

---
+++ app/src/main/java/ch/renklus/android/wetterstation/MainActivity.java
@@ -0,0 +1,76 @@
+package ch.renklus.android.wetterstation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ListView
+        Refresh();
+
+        //Refresh Button
+        final Button refresh = (Button)findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Refresh();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void Refresh() {
+        ListView listView = (ListView) findViewById(R.id.listView);
+        String[] values = GetValues();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        listView.setAdapter(adapter);
+    }
+
+    int i = 0;
+
+    private String[] GetValues()
+    {
+        String[] values =  new String[]
+                {
+                        "",
+                        "Android List View",
+                        "Simple List View in Android",
+                        "Create List View Android",
+                        "Android Example",
+                };
+        values[0] = Integer.toString(++i);
+        return values;
+    }
+}
